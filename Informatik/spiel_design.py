fb3902b9d9f09f715798d3f8958c50e1a5c6595c
safouan <safomohammed@gmail.com>
2026-06-10 14:32:32 +0200

    Python Skript fuer farbige Systemmeldungen hinzugefuegt

---
+++ Informatik/spiel_design.py
@@ -0,0 +1,11 @@
+# Unsere Farb-Werkzeuge
+ROT = "\033[31m"
+GRUEN = "\033[32m"
+RESET = "\033[0m" # Schaltet die Farbe wieder aus
+
+# Beispiel: Erfolg in Grün
+print(GRUEN + "Erfolg: Du hast das Level bestanden!" + RESET)
+
+
+#rote Zeile für Fehler
+print(ROT + "Fehler: Die Systemmeldung ist fehlgeschlagen!" + RESET)
